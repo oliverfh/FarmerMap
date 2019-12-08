2fb15597c76d7e73850fc0a0654c8bf088661df3
dark <toxicleague007@gmail.com>
2019-12-08 12:17:57 -0500

    Starting jquery implementation

--- posts.js
+++ posts.js
@@ -1,23 +1,29 @@
 function renderPost() {
     let head = ``
     head = `
-    <div class="renderTweets">
-    <div class="card">
-    <div style="background-color:blue">
-        <div class="card-image">
-            <h1 style="color:white" id="author">Author Here</h1>
+    <div class="card" align="center">
+      <div class="card-content">
+        <div class="media">
+          <div class="media-content">
+            <p class="title is-4" align="center">John Smith</p>
+            <p class="subtitle is-6" align="center">@johnsmith</p>
+          </div>
         </div>
+    
+        <div class="content">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+          <a href="#">#css</a> <a href="#">#responsive</a>
+          <br>
+          <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
         </div>
-        <p class="card-content" text-align:"center">Listing here</p>
-
-        <div class="btn-group">
-        <button id="like">Like: </button>
-        <button id="retweet">Retweet: </button>
-        <button id="reply">Reply</button>
-      </div> 
-    </div>
+      </div>
     </div>
     `
    
     return head;
+}
+
+for(let i = 0; i < 50; i++) {
+    $("#posts").append(renderPost());
 }
