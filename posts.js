362dd024b5ebda7c3eb2e7ea2d750e1dc6c7aba3
Leon Bauer <lbauer822@gmail.com>
2019-12-10 14:47:49 -0500

    Added buttons

--- posts.js
+++ posts.js
@@ -18,14 +18,12 @@ function renderPost() {
             <p class="title is-4" align="center">John Smith</p>
             <p class="subtitle is-6" align="center">@johnsmith</p>
           </div>
-        </div>
-    
+        </div>    
         <div class="content">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit.
           Phasellus nec iaculis mauris. <a>@bulmaio</a>.
           <a href="#">#css</a> <a href="#">#responsive</a>
           <br>
-          <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
         </div>
       </div>
     </div>
@@ -37,20 +35,19 @@ function makeListing(e) {
   let head = `
   <div id="#postCenter",class="card" align="center">
     <div class="card-content">
-  
       <div id="toSell",class="content">
-        <span>Write your listing</span>
         <h3>What are you selling?</h3>
-        <input id="title",type = text></input>
+        <textarea id="title",type = text></textarea>
         <h3>Give a description of your produce</h3>
         <textarea id="description"></textarea>
         <h3>Street Address</h3>
-        <input id="streetad",type=text></input>
+        <input id="streetad",type="text"></input>
         <h3>State(Abbreviation)</h3>
         <input id="state",type="text"></input>
         <h3>ZIP Code</h3>
         <input id="zip",type="text"></input>
         <br>
+        <button id="listingsubmit" type="submit">Post</button>
       </div>
     </div>
   </div>
