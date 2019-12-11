d16edec9ce97da39bced7e78a7e52d6b5c1316c7
Rei Banerjee <rei.banerjee@gmail.com>
2019-12-11 12:45:37 -0500

    profile page markup done

--- profile.js
+++ profile.js
@@ -1,38 +1,65 @@
-function renderProfile() {
-    let head = ``
-    head = `
-    <div class="card" align="center">
-      <div class="card-content">
-        <div class="media">
-          <div class="media-content">
-            <p class="title is-4" align="center">(Name)</p>
-            <p class="subtitle is-6" align="center">@(username)</p>
-            <p class="subtitle is-6" align="center">Email: (email)</p>
-          </div>
-        </div>
-    
-        <div class="content">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          Phasellus nec iaculis mauris.
-          <br>
-          <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
-        </div>
+function renderProfile(data) {
+  let head =`
+  <div class="card" align="center">
+  <div class="card-content">
+    <div class="media">
+      <div class="media-content">
+        <p class="title is-4" align="center">${data.fullName}</p>
+        <p class="subtitle is-6" align="center">@${data.userName}</p>
       </div>
+    </div>    
+    <div class="content">
+    <div>
+      <h6 class="cardsubtitles">What I'm Selling</h6>
+      <p>${data.title}</p>
     </div>
-    `
-   
-    return head;
+    <div>
+      <h6 class="cardsubtitles">Description:</h6>
+      <p>${data.description}</p>
+    </div>
+    <div>
+      <h6 class="cardsubtitles">Address</h6>
+      <p>${data.streetad}</p>
+    </div>
+    <div>
+      <h6 class="cardsubtitles">City: </h6>
+      ${data.city}, ${data.state} ${data.zip}
+      ${data.datetime}
+    </div>
+      <br/>
+      <button id="listinglike">Like</button>
+      <button id="listingsave">Save</button>
+      <button id="listingedit">Edit</button>
+      <button id="listingdelete">Delete</button>
+    </div>
+  </div>
+</div>
+
+  `
+  $('#userprofile').prepend(head);
 }
 
-$("#userprofile").append(renderProfile());
 
 
-function onLoad() {
+async function onLoad() {
+  let token = localStorage.getItem('token')
+
   $('#log').on('click', '#logoutbutton',logout)
+  const result = await axios({
+    method: 'GET',
+    url: 'http://localhost:3000/user/posts',
+    headers: {
+      Authorization: "Bearer " + token
+    }
+});
+//console.log(result.data);
+for(let i=0; i<result.data.result.length;i++) {
+  renderProfile(result.data.result[i]);
+
+}
 }
 
 function logout() {
-  let token = localStorage.getItem('token')
   //console.log(token);
   localStorage.removeItem('token');
  // console.log(localStorage.getItem('token'));
