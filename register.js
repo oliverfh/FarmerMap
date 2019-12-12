7d85061ee96fc0352d05d57385e5459f2c3895be
Rei Banerjee <rei.banerjee@gmail.com>
2019-12-11 23:17:17 -0500

    fixed .

--- register.js
+++ register.js
@@ -18,6 +18,7 @@ async function makeAccount(e) {
     const name = $('#name').val();
     const username = $('#username').val();
     const pass = $('#pass').val();
+    const quote ='';
     //todo: add indication that account trying to be made has already been registered?
         const result = await axios({
             method: 'POST',
@@ -27,7 +28,7 @@ async function makeAccount(e) {
                 "pass": pass,
                 "data": {
                     "fullName": name,
-                    "quote": ''.
+                    "quote": quote,
                 }
             }
         });
